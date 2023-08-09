4ff254fe9bde7fae9638a0fa871cad9b03eed3e5
Divya Sai Iruku <137642497+Divya-i@users.noreply.github.com>
2023-08-09 13:25:41 +0530

    Add files via upload

---
+++ design.v
@@ -0,0 +1,20 @@
+// Code your design here
+module all_logic_gates(input a, b,
+                       output not_gate,
+                       output and_gate,
+                       output or_gate,
+                       output nand_gate,
+                       output nor_gate,
+                       output xor_gate,
+                       output xnor_gate);
+  
+  not n1(not_gate,a);
+  and a1(and_gate,a,b);
+  or o1(or_gate,a,b);
+  nand na1(nand_gate,a,b);
+  nor no1(nor_gate,a,b);
+  xor x1(xor_gate,a,b);
+  xnor xn1(xnor_gate,a,b);
+  
+endmodule
+   
