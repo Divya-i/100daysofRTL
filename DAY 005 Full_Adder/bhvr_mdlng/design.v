dec8cc8e8f3c5ba88dc05b6224fef87b13113119
Divya Sai Iruku <137642497+Divya-i@users.noreply.github.com>
2023-09-04 15:39:53 +0530

    Day 5

---
+++ DAY 005 Full_Adder/bhvr_mdlng/design.v
@@ -0,0 +1,15 @@
+// Code your design here
+// Code your design here
+module full_addr(a,b,cin,sum,cout);
+input a,b,cin;
+output reg sum,cout;
+  
+  always@(*)
+    begin
+      
+      sum  = a ^ b ^ cin;
+      cout = (a & b) | (b & cin) | (a & cin) ;
+   
+    end
+endmodule
+
